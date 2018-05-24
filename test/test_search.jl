09715ea8a9f128363532ac66cfea6def2cfbe910
Richard Guo <richardkwo@gmail.com>
2018-05-23 23:24:35 -0700

    bug fix

--- test/test_search.jl
+++ test/test_search.jl
@@ -2,12 +2,11 @@ using Base.Test
 using StatsBase.sample
 using DataFrames
 
-X = readdlm(joinpath(@__DIR__, "X1.dat"))
-env = Vector{Int}(X[:,1])
-X = X[:,2:end]
-S = 1:size(X,2)
-
 @time @testset "causal search" begin
+    X = readdlm(joinpath(@__DIR__, "X1.dat"))
+    env = Vector{Int}(X[:,1])
+    X = X[:,2:end]
+    S = 1:size(X,2)
     result = causalSearch(X, X[:, 1], env, setdiff(S,1) , α=0.01)
     @test result.model_reject == true
     result = causalSearch(X, X[:, 2], env, setdiff(S,2) , α=0.01, p_max=4)
@@ -19,6 +18,7 @@ end
                 x2 = [0.0  0.0  1.0  0.0  0.0  0.0  1.0  1.0  1.0  0.0  1.0  0.0  0.0  0.0  1.0  1.0][:],
                 x3 = [0.0  0.0  1.0  1.0  0.0  0.0  1.0  0.0  0.0  0.0  1.0  1.0  0.0  0.0  1.0  0.0][:],
                 y =  [1.0  1.0  1.0  0.0  0.0  1.0  1.0  1.0  0.0  1.0  1.0  0.0  0.0  1.0  1.0  0.0][:])
+    env = repeat([1,2], inner=8)
     r1 = causalSearch(df, :y, env, method="logistic-LR", iterate_all=true)
     @test length(r1.S) == 0
     r2 = causalSearch(df, :y, env, method="logistic-BF", iterate_all=true)
